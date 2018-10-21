80c781782c0987e3913e75b62e138823a5d0655f
DAdeRijk <damian04@msn.com>
2018-10-21 15:22:53 +0200

    Update HU Programming classes/PROG miniproject groep 4/base API-function.py
    No internet connection error added

--- HU Programming classes/PROG miniproject groep 4/base API-function.py
+++ HU Programming classes/PROG miniproject groep 4/base API-function.py
@@ -2,25 +2,28 @@ import requests
 import xmltodict
 
 def GetDepartures(StationName):
-    authDetails = ('mees-overbeek@hotmail.com', 'xyy3hgLOXBq1i3sNZgp5qVwLPraeb4APZw_JI2iEB-zP3qfpfsj9fg')
-    apiUrl = 'http://webservices.ns.nl/ns-api-avt?station=' + StationName
-    response = requests.get(apiUrl, auth = authDetails)
-    vertrekXML = xmltodict.parse(response.text)
+    try:
+        authDetails = ('mees-overbeek@hotmail.com', 'xyy3hgLOXBq1i3sNZgp5qVwLPraeb4APZw_JI2iEB-zP3qfpfsj9fg')
+        apiUrl = 'http://webservices.ns.nl/ns-api-avt?station=' + StationName
+        response = requests.get(apiUrl, auth = authDetails)
+        vertrekXML = xmltodict.parse(response.text)
 
-    if "error" in vertrekXML.keys():
-        print(vertrekXML["error"]["message"])
-        exit()
+        if "error" in vertrekXML.keys():
+            print(vertrekXML["error"]["message"])
+            return
 
-    print('dit zijn de vertrekkende treinen: ')
+        print('dit zijn de vertrekkende treinen: ')
 
-    for vertrek in vertrekXML['ActueleVertrekTijden']['VertrekkendeTrein']:
-        eindbestemming = vertrek['EindBestemming']
+        for vertrek in vertrekXML['ActueleVertrekTijden']['VertrekkendeTrein']:
+            eindbestemming = vertrek['EindBestemming']
 
-        vertrektijd = vertrek['VertrekTijd']
+            vertrektijd = vertrek['VertrekTijd']
 
-        vertrektijd = vertrektijd[11:16]
+            vertrektijd = vertrektijd[11:16]
 
-        print('Om ' + vertrektijd + ' vertrekt een trein naar ' + eindbestemming)
+            print('Om ' + vertrektijd + ' vertrekt een trein naar ' + eindbestemming)
+    except:
+        print("No internet connection")
 
 
-GetDepartures("Utrecht")
+GetDepartures("Utrect")
