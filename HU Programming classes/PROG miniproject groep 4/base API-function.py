ea51900557788d6002124af944ffb02a3020cbe7
DAdeRijk <damian04@msn.com>
2018-10-21 14:33:20 +0200

    Update HU Programming classes/PROG miniproject groep 4/base API-function.py
    Created GetDepartures function with a parameter of the current station

--- HU Programming classes/PROG miniproject groep 4/base API-function.py
+++ HU Programming classes/PROG miniproject groep 4/base API-function.py
@@ -1,22 +1,26 @@
 import requests
 import xmltodict
 
-authDetails = ('mees-overbeek@hotmail.com', 'xyy3hgLOXBq1i3sNZgp5qVwLPraeb4APZw_JI2iEB-zP3qfpfsj9fg')
-apiUrl = 'http://webservices.ns.nl/ns-api-avt?station=utrecht'
-response = requests.get(apiUrl, auth = authDetails)
-vertrekXML = xmltodict.parse(response.text)
+def GetDepartures(StationName):
+    authDetails = ('mees-overbeek@hotmail.com', 'xyy3hgLOXBq1i3sNZgp5qVwLPraeb4APZw_JI2iEB-zP3qfpfsj9fg')
+    apiUrl = 'http://webservices.ns.nl/ns-api-avt?station=' + StationName
+    response = requests.get(apiUrl, auth = authDetails)
+    vertrekXML = xmltodict.parse(response.text)
 
-if "error" in vertrekXML.keys():
-    print(vertrekXML["error"]["message"])
-    exit()
+    if "error" in vertrekXML.keys():
+        print(vertrekXML["error"]["message"])
+        exit()
 
-print('dit zijn de vertrekkende treinen: ')
+    print('dit zijn de vertrekkende treinen: ')
 
-for vertrek in vertrekXML['ActueleVertrekTijden']['VertrekkendeTrein']:
-    eindbestemming = vertrek['EindBestemming']
+    for vertrek in vertrekXML['ActueleVertrekTijden']['VertrekkendeTrein']:
+        eindbestemming = vertrek['EindBestemming']
 
-    vertrektijd = vertrek['VertrekTijd']
+        vertrektijd = vertrek['VertrekTijd']
 
-    vertrektijd = vertrektijd[11:16]
+        vertrektijd = vertrektijd[11:16]
 
-    print('Om ' + vertrektijd + ' vertrekt een trein naar ' + eindbestemming)
+        print('Om ' + vertrektijd + ' vertrekt een trein naar ' + eindbestemming)
+
+
+GetDepartures("Utrecht")
