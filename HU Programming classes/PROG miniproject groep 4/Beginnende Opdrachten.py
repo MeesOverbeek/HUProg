559f9963515d8ea1608240089757cedc89ef0d3c
Bastiaan <bastiaanvanderree@gmail.com>
2018-10-23 14:11:24 +0200

    Update HU Programming classes/PROG miniproject groep 4/Beginnende Opdrachten.py
    error msg

--- HU Programming classes/PROG miniproject groep 4/Beginnende Opdrachten.py
+++ HU Programming classes/PROG miniproject groep 4/Beginnende Opdrachten.py
@@ -13,60 +13,65 @@ def stationClick():
 
 
 def vertrekker():
-    stationName = stationEntry.get()
-    authDetails = ('mees-overbeek@hotmail.com', 'xyy3hgLOXBq1i3sNZgp5qVwLPraeb4APZw_JI2iEB-zP3qfpfsj9fg')
-    apiUrl = 'http://webservices.ns.nl/ns-api-avt?station=' + stationName
-    response = requests.get(apiUrl, auth=authDetails)
+    try:
+        stationName = stationEntry.get()
+        authDetails = ('mees-overbeek@hotmail.com', 'xyy3hgLOXBq1i3sNZgp5qVwLPraeb4APZw_JI2iEB-zP3qfpfsj9fg')
+        apiUrl = 'http://webservices.ns.nl/ns-api-avt?station=' + stationName
+        response = requests.get(apiUrl, auth=authDetails)
 
-    vertrekXML = xmltodict.parse(response.text)
+        vertrekXML = xmltodict.parse(response.text)
+        if "error" in vertrekXML.keys():
+            print(vertrekXML["error"]["message"])
+            messagebox.showerror('Foutmelding',
+                                 'Invoer is ongeldig, kijk of de naam van het station correct is ingevoerd')
+            return
 
-    if "error" in vertrekXML.keys():
-        print(vertrekXML["error"]["message"])
-        messagebox.showerror('Foutmelding','Invoer is ongeldig, kijk of de naam van het station correct is ingevoerd')
-        return
+        departureInformation = "";
+        row = 0;
+        for vertrek in vertrekXML['ActueleVertrekTijden']['VertrekkendeTrein']:
 
-    departureInformation = "";
-    row = 0;
-    for vertrek in vertrekXML['ActueleVertrekTijden']['VertrekkendeTrein']:
+            eindbestemming = vertrek['EindBestemming']
 
-        eindbestemming = vertrek['EindBestemming']
+            vertrektijd = vertrek['VertrekTijd']
 
-        vertrektijd = vertrek['VertrekTijd']
+            vertrektijd = vertrektijd[11:19]
 
-        vertrektijd = vertrektijd[11:19]
+            print(vertrek)
 
-        print(vertrek)
+            vertrektijdlabel = Label(master=departureframe,
+                                     text=vertrektijd,
+                                     background='yellow',
+                                     foreground='black',
+                                     font=('Helvetica', 16, 'bold italic'),
+                                     anchor=W)
+            vertrektijdlabel.grid(row=row, column=0, sticky=N, pady=(8, 0))
+            eindbestemminglabel = Label(master=departureframe,
+                                        text=eindbestemming,
+                                        background='yellow',
+                                        foreground='black',
+                                        font=('Helvetica', 16, 'bold italic'),
+                                        anchor='w')
+            eindbestemminglabel.grid(row=row, column=1, sticky=N, pady=(8, 0))
+            try:
+                vertrekspoorlabel = Label(master=departureframe,
+                                          text="Spoor " + str(vertrek["VertrekSpoor"]["#text"]),
+                                          background='yellow',
+                                          foreground='black',
+                                          font=('Helvetica', 16, 'bold italic'))
+                vertrekspoorlabel.grid(row=row, column=2, sticky=E, pady=(8, 0))
+            except:
+                print("geen spoor")
+            row += 1;
 
+            departureInformation += 'Om ' + vertrektijd + ' vertrekt een trein naar ' + eindbestemming + '\n'
+        mainframe.forget();
+        departureframe.pack(fill=None, expand=False);
+        showinfo(message=departureInformation)
 
-        vertrektijdlabel = Label(master=departureframe,
-              text= vertrektijd,
-              background='yellow',
-              foreground='black',
-              font=('Helvetica', 16, 'bold italic'),
-              anchor=W)
-        vertrektijdlabel.grid(row=row, column=0,sticky=N, pady=(8,0))
-        eindbestemminglabel = Label(master=departureframe,
-              text= eindbestemming,
-              background='yellow',
-              foreground='black',
-              font=('Helvetica', 16, 'bold italic'),
-              anchor='w')
-        eindbestemminglabel.grid(row=row, column=1, sticky=N, pady=(8,0))
-        try:
-            vertrekspoorlabel = Label(master=departureframe,
-                  text= "Spoor " + str(vertrek["VertrekSpoor"]["#text"]),
-                  background='yellow',
-                  foreground='black',
-                  font=('Helvetica', 16, 'bold italic'))
-            vertrekspoorlabel.grid(row=row, column=2, sticky=E, pady=(8,0))
-        except:
-            print("geen spoor")
-        row += 1;
+    except:
+        messagebox.showerror('Foutmelding', 'U bent niet verbonden met het internet, controleer uw internetverbinding en probeer het opnieuw.')
+        print("No internet connection")
 
-        departureInformation += 'Om ' + vertrektijd + ' vertrekt een trein naar ' + eindbestemming + '\n'
-    mainframe.forget();
-    departureframe.pack(fill=None, expand=False);
-    showinfo(message=departureInformation)
 
 
 #form functions to perform commands, functions need to remain outside the tkinter frame code (remain global)
